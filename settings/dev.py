452388eafd282a9bf81f4946e1db38c642617be0
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-03 00:33:10 +0000

    fix static

--- settings/dev.py
+++ settings/dev.py
@@ -194,15 +194,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
