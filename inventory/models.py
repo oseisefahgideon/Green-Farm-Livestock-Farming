6c726ba401ecc9ce4de60985f46b6f411ea919ac
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-18 06:19:41 +0000

    Remove Quantity from product

--- inventory/models.py
+++ inventory/models.py
@@ -16,7 +16,6 @@ class Product(BaseModel):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField()
     image = models.ImageField(upload_to="products/", blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
